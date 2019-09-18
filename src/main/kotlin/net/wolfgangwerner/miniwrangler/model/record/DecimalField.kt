a1cd6db4bc33f49b03622a0f034dccfac27fbe97
wwerner <mail@wolfgang-werner.net>
2019-09-18 13:46:03 +0200

    Make parsing of decimals locale insensitive

--- src/main/kotlin/net/wolfgangwerner/miniwrangler/model/record/DecimalField.kt
+++ src/main/kotlin/net/wolfgangwerner/miniwrangler/model/record/DecimalField.kt
@@ -3,16 +3,22 @@ package net.wolfgangwerner.miniwrangler.model.record
 import java.math.BigDecimal
 import java.text.DecimalFormat
 
-class DecimalField(name: String, val decimalFormat: String) : RecordField<BigDecimal>(name) {
-    val formatter = DecimalFormat(decimalFormat)
-
-    init {
-        formatter.setParseBigDecimal(true)
-    }
+class DecimalField(name: String, val decimalFormat: String, val groupingSeparator: Char = ',', val decimalSeparator: Char = '.') : RecordField<BigDecimal>(name) {
 
     override fun unmarshal(vararg data: String): BigDecimal {
         // TODO: validate
         val value = data[0]
+
+        // This looks like it could be written more concise, but...
+        // * ... DecimalFormat is not thread safe, so we create a new instance for each transformation
+        // * ... if we don't set the separators, the string will be parsed with the ones from the current locale
+        // * ... DecimalFormat.getDecimalFormatSymbols returns a copy, so we need to obtain it, set our values and then update the field
+        val formatter = DecimalFormat(decimalFormat)
+        formatter.isParseBigDecimal = true
+        val symbols = formatter.decimalFormatSymbols
+        symbols.groupingSeparator = groupingSeparator
+        symbols.decimalSeparator = decimalSeparator
+        formatter.decimalFormatSymbols = symbols
         return formatter.parse(value) as BigDecimal
     }
 }
